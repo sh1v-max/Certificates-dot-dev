fb4e14ff5e000a228f85b332c058847f1c460f0d
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-05-11 19:43:55 +0530

    sol 2

---
+++ 02-declaring-variables-challenge-1/src/main.js
@@ -0,0 +1,20 @@
+// Complete the challenge requirements here
+const javascript = 'king'
+console.log(javascript)
+
+console.log(javascript.includes('hard'))
+const jsIsHard = javascript.includes('hard')
+console.log(jsIsHard)
+
+let age = 12
+console.log(age)
+age = 13
+console.log(age)
+
+const isPreppingForExam = true
+console.log(isPreppingForExam)
+
+const isOlderThan8 = age > 8
+console.log(isOlderThan8)
+
+let thisVarHasNoValue
