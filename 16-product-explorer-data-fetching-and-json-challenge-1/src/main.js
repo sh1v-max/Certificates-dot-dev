551dbf44897816c89b71ac4358389c0532f7f102
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-05-18 21:37:02 +0530

    question 17 done

--- 16-product-explorer-data-fetching-and-json-challenge-1/src/main.js
+++ 16-product-explorer-data-fetching-and-json-challenge-1/src/main.js
@@ -13,34 +13,47 @@ const data = await fetchAllProducts()
 // console.log(data.products)
 
 // then render the results to the screen
-const renderedProducts = data.products.map((product) => {
-  return renderProduct(product)
+data.products.forEach((product) => {
+  renderProduct(product)
 })
 
 // 2. Fetch Single Product Details
 
 // define the function so that it fetches the data
 export async function fetchProductDetails (id) {
+  const res = await fetch(`https://dummyjson.com/products/${id}`)
+  if (!res.ok) {
+    return {
+      'error': {
+        'message': res.statusText,
+        'status': res.status
+      }
 
+    }
+  }
+  return await res.json()
 }
 
 // fetch the product with the id of 2
-
+const product = await fetchProductDetails(2)
+console.log(product)
 //  then render it to the screen
-
+renderProduct(product)
 
 // 3. Error Handling
 
 // update the fetchProductDetails to handle a not found response
 
 // test it by a produce with id 9999
+const noExistProduct = await fetchProductDetails(9999)
 
 // render the error messaage
+renderProduct(noExistProduct)
 
 
 // There is no need to change the code here
 function renderProduct (product) {
-  console.log(product)
+  // console.log(product)
   const container = document.getElementById('productContainer')
   const newElement = document.createElement('div')
   if (product.error) {
